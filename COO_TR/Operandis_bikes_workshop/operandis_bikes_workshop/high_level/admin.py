250eb08bfcebc8ce84c98c7212135828a9eca568
Modeus Operandis <Ferrucci_Hugo@proton.me>
2026-09-23 11:56:49 +0200

    add admin file for display

--- COO_TR/Operandis_bikes_workshop/operandis_bikes_workshop/high_level/admin.py
+++ COO_TR/Operandis_bikes_workshop/operandis_bikes_workshop/high_level/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+admin.site.register(models.Pays)
+admin.site.register(models.Ville)
+admin.site.register(models.Point_de_vente)
+admin.site.register(models.Operation)
+admin.site.register(models.Stock)
+admin.site.register(models.Machine)
+admin.site.register(models.Facture)
+admin.site.register(models.Lieu)
+admin.site.register(models.Quantite_machine)
+admin.site.register(models.Transport)
+admin.site.register(models.Produit)
+admin.site.register(models.Prix_Produit)
+admin.site.register(models.QuantiteProduit)
